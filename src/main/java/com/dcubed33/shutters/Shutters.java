a81efbd1f3bf2a69ef68524f97ac49f0b2f3239b
Dcubed33 <dcubed43@gmail.com>
2015-01-10 17:28:44 -0500

    1

---
+++ src/main/java/com/dcubed33/shutters/Shutters.java
@@ -0,0 +1,11 @@
+package com.dcubed33.shutters;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="Shutters", name="Shutters", version="1.7.10-0.1-alpha")
+public class Shutters {
+    @Mod.EventHandler
+    public void preInit(){
+
+    }
+}
