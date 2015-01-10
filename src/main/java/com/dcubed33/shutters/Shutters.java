3d05a490902c6e05ab2b1900ce195f8c4ef2c471
Dcubed33 <dcubed43@gmail.com>
2015-01-10 17:32:03 -0500

    1

--- src/main/java/com/dcubed33/shutters/Shutters.java
+++ src/main/java/com/dcubed33/shutters/Shutters.java
@@ -1,11 +1,24 @@
 package com.dcubed33.shutters;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="Shutters", name="Shutters", version="1.7.10-0.1-alpha")
 public class Shutters {
     @Mod.EventHandler
-    public void preInit(){
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
 
     }
 }
