7820a9b4bdbf05a7aece9d694d2d6eea804ba0e6
Dcubed33 <dcubed43@gmail.com>
2015-01-11 17:56:06 -0500

    Added ModBlocks.java

--- src/main/java/com/dcubed33/shutters/block/ModBlocks.java
+++ src/main/java/com/dcubed33/shutters/block/ModBlocks.java
@@ -1,8 +1,9 @@
 package com.dcubed33.shutters.block;
 
+import com.dcubed33.shutters.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockMain shutters = new BlockShutters();
 
