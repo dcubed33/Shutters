c9648c64cd07b9a7031640e6cb9f4cc3820d9340
Dcubed33 <dcubed43@gmail.com>
2015-01-11 17:57:51 -0500

    Added Blinds Catalog to creatie tab

--- src/main/java/com/dcubed33/shutters/item/ItemBlindsCatalog.java
+++ src/main/java/com/dcubed33/shutters/item/ItemBlindsCatalog.java
@@ -1,9 +1,12 @@
 package com.dcubed33.shutters.item;
 
+import com.dcubed33.shutters.creativetab.CreativeTabShutters;
+
 public class ItemBlindsCatalog extends ItemMain{
     public ItemBlindsCatalog(){
         super();
         this.setUnlocalizedName("blindsCatalog");
         this.maxStackSize = 1;
+        this.setCreativeTab(CreativeTabShutters.Shutters_TAB);
     }
 }
