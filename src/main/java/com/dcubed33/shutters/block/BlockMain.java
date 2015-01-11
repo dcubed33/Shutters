a0fa5b48a6a2ee185efabd9660bc4d1bfe91389f
Dcubed33 <dcubed43@gmail.com>
2015-01-11 13:02:09 -0500

    Added eclipse folder and basic block stuff

---
+++ src/main/java/com/dcubed33/shutters/block/BlockMain.java
@@ -0,0 +1,10 @@
+package com.dcubed33.shutters.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockMain extends Block {
+    public BlockMain(Material material){
+        super(material);
+    }
+}
