b19dd05a1346b5d9755c344d0e2a51bf14ccf181
Dcubed33 <dcubed43@gmail.com>
2015-01-12 17:21:32 -0500

    Added 5 other wood types to mod

--- src/main/java/com/dcubed33/shutters/block/ModBlocks.java
+++ src/main/java/com/dcubed33/shutters/block/ModBlocks.java
@@ -2,12 +2,23 @@ package com.dcubed33.shutters.block;
 
 import com.dcubed33.shutters.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
+import jdk.nashorn.internal.ir.Block;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockMain oakShutter = new BlockOakShutters();
+    public static final BlockMain birchShutter = new BlockBirchShutters();
+    public static final BlockMain spruceShutter = new BlockSpruceShutters();
+    public static final BlockMain jungleShutter = new BlockJungleShutters();
+    public static final BlockMain acadiaShutter = new BlockAcadiaShutters();
+    public static final BlockMain darkOakShutter = new BlockDarkOakShutters();
 
     public static void init(){
         GameRegistry.registerBlock(oakShutter, "oakShutters");
+        GameRegistry.registerBlock(birchShutter, "birchShutters");
+        GameRegistry.registerBlock(spruceShutter, "spruceShutters");
+        GameRegistry.registerBlock(jungleShutter, "jungleShutters");
+        GameRegistry.registerBlock(acadiaShutter, "acadiaShutters");
+        GameRegistry.registerBlock(darkOakShutter, "darkOakShutters");
     }
 }
