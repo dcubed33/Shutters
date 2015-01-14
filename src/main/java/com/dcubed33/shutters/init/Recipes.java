e34bca7ea577d19a954af74488777f5d64d1ae55
Dcubed33 <dcubed43@gmail.com>
2015-01-13 19:10:55 -0500

    Added recipes for all shutters

--- src/main/java/com/dcubed33/shutters/init/Recipes.java
+++ src/main/java/com/dcubed33/shutters/init/Recipes.java
@@ -6,11 +6,17 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class Recipes {
     public static void init(){
-        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.oakShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks), 'y', new ItemStack(Blocks.wooden_slab));
-        GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(ModItems.shuttersCatalog), "blockShutter"));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.oakShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 0), 'y', new ItemStack(Blocks.wooden_slab, 1, 0));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.spruceShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 1), 'y', new ItemStack(Blocks.wooden_slab, 1, 1));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.birchShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 2), 'y', new ItemStack(Blocks.wooden_slab, 1 , 2));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.jungleShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 3), 'y', new ItemStack(Blocks.wooden_slab, 1, 3));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.acaciaShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 4), 'y', new ItemStack(Blocks.wooden_slab, 1, 4));
+        GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.darkOakShutter), "xyx", "x x", "xyx", 'x', new ItemStack(Blocks.planks, 1, 5), 'y', new ItemStack(Blocks.wooden_slab, 1, 5));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.shuttersCatalog), "pxx", "pxx", "pxx", 'x', "blockShutter", 'p', new ItemStack(Items.paper)));
     }
 }
