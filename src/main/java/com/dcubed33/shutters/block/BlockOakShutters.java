bea6dab6b12ec201cb9a16fdf0856cb8f19ce0b4
Dcubed33 <dcubed43@gmail.com>
2015-01-16 19:03:58 -0500

    Attempt at making item drop

--- src/main/java/com/dcubed33/shutters/block/BlockOakShutters.java
+++ src/main/java/com/dcubed33/shutters/block/BlockOakShutters.java
@@ -1,6 +1,15 @@
 package com.dcubed33.shutters.block;
 
 import com.dcubed33.shutters.creativetab.CreativeTabShutters;
+import com.dcubed33.shutters.init.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockOakShutters extends BlockMain{
     public BlockOakShutters(){
@@ -12,7 +21,26 @@ public class BlockOakShutters extends BlockMain{
         this.setHardness(0.8F);
         this.setStepSound(soundTypeWood);
     }
+
+    @Override
     public boolean isOpaqueCube(){
         return false;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock(){
+        return false;
+    }
+
+    @Override
+    public Item getItemDropped(int int1, Random random, int int2)
+    {
+        return Item.getItemFromBlock(ModBlocks.oakShutter);
+    }
+
+    @Override
+    public int quantityDropped(Random random)
+    {
+        return 1;
     }
 }
